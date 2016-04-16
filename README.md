65cd9c19d80bb78c1ed07d32c30da4463b91c6b0
Lisa Xie <Lisa Xie>
2016-04-16 23:09:38 +0100

    played around with web.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DemocraticParty
+
+To start Stan's node front-end project
+1. cd into the project folder
+2. "npm start"
+3. go to localhost:3000
--- website/web.py
+++ website/web.py
@@ -1,23 +1,67 @@
 # all the imports
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 # create our little application :)
 app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE= './base.db',
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
 app.config.from_object(__name__)
 
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-    
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.before_request
+def before_request():
+    init_db()
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+        
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select party from parties')
+    entries = [dict(party=row[1]) for row in cur.fetchall()]
+    console.log(entries)
+    return render_template('show_entries.html', entries=entries)
+
 if __name__ == '__main__':
     app.run()
+    
